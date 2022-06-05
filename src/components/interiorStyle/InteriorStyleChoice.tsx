bf1055e50f38fc221f461bcdb3addf716666b07f
kamil <kami.czyzewski96@gmail.com>
2022-06-05 20:08:18 +0200

    Added images on 3rd section

--- src/components/interiorStyle/InteriorStyleChoice.tsx
+++ src/components/interiorStyle/InteriorStyleChoice.tsx
@@ -1,13 +1,33 @@
 import "./styles/interiorStyleChoice.css";
+import loft from "../../imgs/loft.png";
+import minimalistyczny from "../../imgs/minimalistyczny.png";
+import nowoczesny from "../../imgs/nowoczesny.png";
+import skandynawski from "../../imgs/skandynawski.png";
+import { useState } from "react";
 
 const InteriorStyleChoice: React.FC = () => {
+    const [shownImage, setShownImage] = useState<string>(loft);
+
+    const images = [{ img: loft, name: "loft" }, { img: minimalistyczny, name: "minimalistyczny" }, { img: nowoczesny, name: "nowoczesny" }, { img: skandynawski, name: "skandynawski" }]
+
     return (
         <div className="interiorStyleChoiceDiv">
             <h1>Krok 2. Wybierz styl aranżacji wnętrza</h1>
 
             <div className="interiorChoiceImages">
-                <div className="interior"></div>
-                <div className="interiorChoice"></div>
+                <div className="interior">
+                    <img src={shownImage} />
+                </div>
+                <div className="interiorChoice">
+                    {images.map((img) => {
+                        return (
+                            <div onClick={() => setShownImage(img.img)} className="imgDiv">
+                                <img src={img.img} />
+                                <p className="interiorDescription">{img.name}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
