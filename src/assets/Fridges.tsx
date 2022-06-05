ee7f369e2044f3aba8c560e3ef73458b27ce06c7
kamil <kami.czyzewski96@gmail.com>
2022-06-05 20:30:42 +0200

    Styling fixes

--- src/assets/Fridges.tsx
+++ src/assets/Fridges.tsx
@@ -7,6 +7,6 @@ export const Fridges = [
     id: ""
   },
   { name: "Lodówka", width: 80, height: 170, color: "white", id: "" },
-  { name: "Zamrażarka", width: 80, height: 170, color: "white", id: "" },
+  { name: "Zamrażarka", width: 70, height: 170, color: "white", id: "" },
   { name: "Lodówka Slim", width: 50, height: 170, color: "white", id: "" },
 ];
